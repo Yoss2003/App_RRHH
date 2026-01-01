8d91526684440cd94305478354aea6c571ee05dc
Alexandro <mercadogarciaalexandro10@gmail.com>
2025-12-31 22:24:41 -0500

    Versión alpha: Creación de archivos base.

---
+++ App_RRHH/Vistas/VistasConfiguraciones/VistaConfiguracionPDF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_RRHH.Vistas.VistasConfiguraciones
+{
+    public partial class VistaConfiguracionPDF : Form
+    {
+        public VistaConfiguracionPDF()
+        {
+            InitializeComponent();
+        }
+    }
+}
