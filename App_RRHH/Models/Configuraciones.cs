c273917a692c5edf1ab8b74cc86d0bb72957a40a
Alexandro <mercadogarciaalexandro10@gmail.com>
2026-01-01 16:50:19 -0500

    Validaciones de inicio de BD
    Se realizó la creación de la conexión a la BD y se creo un JSON para guardar las configuraciones de la app y controlar la primera ejecución de la app.

---
+++ App_RRHH/Models/Configuraciones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace App_RRHH.Models
+{
+    public class Configuraciones
+    {
+        //Configuración general
+        public bool RedimensionarColumnas { get; set; }
+        public bool MostrarGrid { get; set; }
+        public bool OrdenarColumnas { get; set; }
+        public bool HoverLoad { get; set; }
+        public bool MostrarInfo { get; set; }
+
+        //Configuración inicio
+        public string NombreEmpleado { get; set; }
+        public string UsuarioEmpleado { get; set; }
+        public string CorreoAsociado { get; set; }
+        public string ServidorCorreo { get; set; }
+        public string CuerpoCorreo { get; set; }
+        public string RutaOrigen { get; set; }
+        public string RutaDestino { get; set; }
+        public int PuertoCorreo { get; set; }
+        public DateTime FechaActual { get; set; }
+
+        //Configuración reportes
+        public bool BusquedaTiempoReal { get; set; }
+        public bool ColoresEstado { get; set; }
+        public bool FiltroDinamico { get; set; }
+        public bool MostrarContador { get; set; }
+        public bool ExportarAvanzado { get; set; }
+    }
+}
--- App_RRHH/Models/Empleados.cs
+++ App_RRHH/Models/Empleados.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace App_RRHH.Models
 {
-    internal class Empleados
+    namespace Envio_Correos_Masivos.Models
     {
+        public class Empleado
+        {
+            public int IdEmpleado { get; set; }
+            public string NombreCompleto { get; set; }
+
+            public int? Edad { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+            public string Telefono { get; set; }
+            public string Direccion { get; set; }
+            public string Correo { get; set; }
+            public string JefeDirecto { get; set; }
+            public string Area { get; set; }
+            public string Cargo { get; set; }
+            public string TelefonoEmergencia { get; set; }
+            public decimal? Remuneracion { get; set; }
+            public string TipoContrato { get; set; }
+            public DateTime? FechaIngreso { get; set; }
+            public DateTime? FechaTermino { get; set; }
+            public DateTime? FechaCese { get; set; }
+            public string TiempoTrabajado { get; set; }
+        }
     }
+
 }
